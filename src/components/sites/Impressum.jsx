f9407d1cf56928a2fa01a69a15d3a1d7af75359c
FeedThePiano <jokup@t-online.de>
2020-03-02 12:04:52 +0100

    Updates

--- src/components/sites/Impressum.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Impressum() {
-    return (
-        <div style={{height: "calc(100vh - 150px"}}>
-            
-        </div>
-    )
-}
--- src/components/sites/SpecialPlaces.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function SpecialPlaces(props) {
-    return (
-        <div {...props}>
-            
-        </div>
-    )
-}
