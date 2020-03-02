4d822106d73716fae578778873954cdf2f85a1db
FeedThePiano <jokup@t-online.de>
2020-03-02 13:13:43 +0100

    updated build

---
+++ build/precache-manifest.1862186429f4075598e1f21ef9befb0f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1116b22d5240fc22b7d2aae07299c7ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed435f1600314b4a2560",
+    "url": "/static/css/main.8a85c393.chunk.css"
+  },
+  {
+    "revision": "861b3b5d2f0c49e34e3b",
+    "url": "/static/js/2.48512002.chunk.js"
+  },
+  {
+    "revision": "ed435f1600314b4a2560",
+    "url": "/static/js/main.cc9d1a9d.chunk.js"
+  },
+  {
+    "revision": "c1a77f427ae08317b6e1",
+    "url": "/static/js/runtime-main.3c9bfb20.js"
+  },
+  {
+    "revision": "9b550f15f8938c843c72650d7bad6034",
+    "url": "/static/media/BigJohnPRO-Regular.9b550f15.otf"
+  },
+  {
+    "revision": "4005f3c7da0ba7baaeae07f25e8635cc",
+    "url": "/static/media/bg.4005f3c7.jpg"
+  },
+  {
+    "revision": "444d79bbd2ad217f0fa9ef74ddeddf08",
+    "url": "/static/media/logo-font.444d79bb.png"
+  }
+]);
